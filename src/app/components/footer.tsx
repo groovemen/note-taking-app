15806c923f6087df68f1cee955534e96fd8da30f
Stefan <moldovanste@gmail.com>
2024-11-22 19:36:10 +0200

    Update footer.tsx

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -8,7 +8,7 @@ const Footer = (props: Props) => {
     <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -19,11 +19,11 @@ const Footer = (props: Props) => {
             width={16}
             height={16}
           />
-          Learn
+          Click Here
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -34,7 +34,7 @@ const Footer = (props: Props) => {
             width={16}
             height={16}
           />
-          Examples
+          And Here
         </a>
       </footer>
   )
