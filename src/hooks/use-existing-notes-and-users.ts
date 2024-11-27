7a1671ae1e3dbf3c1301102aff469ee5bc8b0404
Stefan <moldovanste@gmail.com>
2024-11-27 18:16:07 +0200

    refactor: update and add new comments

--- src/hooks/use-existing-notes-and-users.ts
+++ src/hooks/use-existing-notes-and-users.ts
@@ -5,40 +5,46 @@ const SESSION = 'challenge_surfe_sesh';
 const BASE_URL = `https://challenge.surfe.com/${SESSION}`;
 const USERS_URL = 'https://challenge.surfe.com/users';
 
+// Type deffinition for Users
 interface Users {
   id: number;
   first_name: string;
 }
 
 export function useExistingData() {
+  // State management
   const [id, setID] = useState<number | null>(null);
   const [note, setNote] = useState<string>("");
   const [users, setUsers] = useState<Users[]>([]);
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
 
+  // Fetch initial data when component mounts
   useEffect(() => {
     const fetchSessionData = async () => {
       try {
+        // Set loading state before data retrieval
         setIsLoading(true);
 
-        // Fetch the Existing Notes
+        // Retrieve existing notes for the current session
         const notes = await fetchData<{ id: number; body: string }[]>(
           `${BASE_URL}/notes`
         );
-
+        
+        // Populate note data if existing notes are found
         if (notes.length > 0) {
           const latestNote = notes[notes.length - 1];
           setNote(latestNote.body);
           setID(latestNote.id);
         }
 
-        // Fetch the Existing Users
+        // Fetch Users list for collaborative features
         const users = await fetchData<Users[]>(USERS_URL);
         setUsers(users);
 
         setIsLoading(false);
       } catch (err: unknown) {
+        // Error handling
         if (err instanceof Error) {
           setError(err.message);
         } else {
@@ -48,8 +54,10 @@ export function useExistingData() {
       }
     };
 
+    // Executes data fetching
     fetchSessionData();
-  }, []);
+  }, []); // Ensure single execution on component mount
 
+  // Return states and setters for component integration
   return { id, note, users, error, isLoading, setID, setNote };
 }
